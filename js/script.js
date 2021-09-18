cc974171ebfd896c4f2c942ffb4c14ceecbef09d
Bach Le <bachlamsung123@gmail.com>
2021-09-18 21:11:31 +0700

    demo v1.0

---
+++ js/script.js
@@ -0,0 +1,97 @@
+
+const initialNav = {
+   "background-color": "transparent",
+   "padding": "2rem 2.5rem",
+   "box-shadow": "none"
+}
+
+const scrolledNav = {
+   "background-color": "rgba(0,0,0,0.9)",
+   "padding": "0.5rem 2.5rem",
+   "box-shadow": "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px"
+}
+
+// Navbar
+$(document).scroll(function() {
+   const scroll = $(window).scrollTop(); 
+   if (scroll < 10) {
+      $(".nav-bar").css(initialNav);
+
+   } else {
+      $(".nav-bar").css(scrolledNav);
+   }
+})  
+
+$('.hamburger').on('click', function() {
+   $('.hamburger').toggleClass('is-active');
+   $('.menu').toggleClass('is-active');
+   $('.nav-bar').toggleClass('is-active');
+})
+
+function removeActive() {
+   $(".option").each(function(){
+      $(this).removeClass("is-active");
+   })
+}
+
+function addActive(number) {
+   $(`.menu :nth-child(${number})`).addClass("is-active");
+}
+
+const level = [0, 550, 1300, 2000, 3100, 100000];
+
+$(document).scroll(function() {
+   const scroll = $(window).scrollTop(); 
+
+   for (let i = 0; i < level.length - 1; i++) {
+      if (scroll > level[i] && scroll < level[i+1]) {
+         removeActive();
+         addActive(i+1);
+      }
+   }
+});
+
+// Item Appear 
+
+$(document).scroll(function() {
+   const scroll = $(window).scrollTop();
+   if (scroll > 350) {
+      $(".mark:nth-child(1)").css("display", "block");
+   }
+
+   if (scroll > 550) {
+      $(".mark:nth-child(2)").css("display", "block");
+   }
+
+});
+
+
+// Slide 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
