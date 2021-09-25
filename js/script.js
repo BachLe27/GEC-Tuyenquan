f3cabb2f1bd631a4c28a781a44db59c8725bbf03
Bach Le <bachlamsung123@gmail.com>
2021-09-25 09:53:58 +0700

    fix count-down responsive size

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ function showSlides(n) {
 }
 
 // Count-down timer 
-const countDownDate = new Date("Oct 5, 2021 11:00:10").getTime();
+const countDownDate = new Date("Oct 1, 2021 11:00:10").getTime();
 
 const count = setInterval(() => {
       // Get today's date and time
